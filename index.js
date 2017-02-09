faa432bfedf1600b24a717dd12715f2bd74aab15
anmatika <4ntti.matikainen@gmail.com>
2017-02-09 09:51:56 +0200

    use native promise

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var crypto = require('crypto');
-var fs = require('fs');
-var q = require('q');
+const crypto = require('crypto');
+const fs = require('fs');
 const chalk = require('chalk');
 const file = process.argv[2];
 
@@ -14,30 +13,30 @@ getSha1(file);
 getSha256(file);
 
 function getMd5(file) {
-    var algo = 'md5';
+    const algo = 'md5';
     getHashOfFile(algo, file)
         .then(d => console.log(chalk.gray('Md5: ') + chalk.green(d)));
 }
 function getSha1(file) {
-    var algo = 'sha1';
+    const algo = 'sha1';
     getHashOfFile(algo, file)
         .then(d => console.log(chalk.gray('Sha1: ') + chalk.green(d)));
 }
 function getSha256(file) {
-    var algo = 'sha256';
+    const algo = 'sha256';
     getHashOfFile(algo, file)
         .then(d => console.log(chalk.gray('Sha256: ') + chalk.green(d)));
 }
 function getHashOfFile(algo, file){
-    const deferred = q.defer();
-    var shasum = crypto.createHash(algo);
-
-    var s = fs.ReadStream(file);
-    s.on('data', function(d) { shasum.update(d); });
-    s.on('end', function() {
-        const d = shasum.digest('hex');
-        deferred.resolve(d);
+    const promise = new Promise((resolve, reject) => {
+        const shasum = crypto.createHash(algo);
+        const s = fs.ReadStream(file);
+        s.on('data', function(d) { shasum.update(d); });
+        s.on('end', function() {
+            const d = shasum.digest('hex');
+            resolve(d);
+        });
     });
 
-    return deferred.promise;
+    return promise;
 }
